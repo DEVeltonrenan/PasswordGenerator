5435a2eafb91c22ff31ecff29af78b545f118b8e
Elton Renan Macedo <renandrunline@gmail.com>
2025-02-11 15:54:17 -0400

    separei em arquivos separados o gerador com a base de dados, com o index.

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,12 @@
 const express = require('express')
-const crypto = require('crypto') //para gerar numeros aleatorios com segurança
-
+const generatecode = require ('./untils/generator')
 
 
 const app = express()
 app.use(express.json());
 const port = 3000
 
-//BASE DE DADOS DE CARACTERES//
 
-const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowercase = 'abcdefghijklmnopqrstuvwxyz';
-const numbers = '0123456789';
-const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
-
-
-
-
-
-    
 
 app.get('/senhas', (req, res) => {
   res.send(senhas)
---
+++ backend/untils/generator.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto') //para gerar numeros aleatorios com segurança
+
+
+//base de dados 
+
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const specialChars = '!@#$%^&*()_+[]{}|;:,.<>?';
+
+
+
+
+
+
+
+
+
+//aqui irá as funções para  gerar a senha aleatoria//
+
