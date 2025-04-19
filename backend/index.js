2a90c68d6202b27fc47e6b10b06ba2ac26689197
Elton Renan Macedo <renandrunline@gmail.com>
2025-04-19 19:16:01 -0400

    Check (#7)
    * testando api em teste generator
    
    * teste de API ok, instalamos o CORS porque o navegador estava bloqueando o acesso por um index.html tester
    
    * ajustes de menssagem de erro, e reflexão para salvar as senhas e etc...
    
    * upei o index antigo

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,37 @@
-const express = require('express')
-const generatecode = require ('./untils/generator')
+const express = require('express');
+const cors = require('cors');
+const generateCode = require('./utils/generator');
 
+const app = express();
 
-const app = express()
+app.use(cors());
+
 const port = 3000;
 
 app.use(express.json());
 
+app.post('/generate', (req, res) => {
+  try {
+    const { uppercase, lowercase, numbers, specialChars, length } = req.body; //pega a informação do corpo da requisição HTML
+    if (characters === none) {
+      return res.status(400).json({ error: 'Ao menos uma caixa deve ser selecionada' });
+    }//verifica se foi selecionado algum tipo de caracteres
 
+    // NÃO ESTÁ FUNCIONANDO COORETAMENTE, AS MENSSAGENS NÃO ESTÃO SENDO EXIBIDAS!!!
 
-app.post('/generate', (req, res) =>   {   
-  try {const {upercase, lowercase, numbers, specialChars, length} = req.body;
-  if (!length || length < 1) {  return res.status(400).json({ error: 'O tamanho deve ser maior que zero.'}); 
-  }
+    //verificar//
 
-  const code = generatecode({upercase, lowercase, numbers, specialChars, length});
+    if (length < 4) {
+      return res.status(400).json({ error: 'O tamanho mínimo da senha é 4 caracteres' });
+    }
+
+    const code = generateCode({ uppercase, lowercase, numbers, specialChars, length });
     res.json({ code });
-} catch (error) {
-  res.status(500).json({ error: 'Erro interno' });
-}
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ' });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
